ef186db465c1b3863d280c4a95ff02013466e930
Rafael Guterres Jeffman <rjeffman@redhat.com>
2024-11-28 18:27:18 -0300

    raspios: Relay on custom.toml for configuration
    Do not uset 'ssh' and 'userconf.txt' files for configuration as
    Raspberry OS based on Bookworm can do both with custom.toml.

--- lib/raspios.sh
+++ lib/raspios.sh
@@ -44,22 +44,11 @@ country = ""
 timezone = "${timezone}"
 EOF
 
-    # enable ssh
-    log_debug "Creating: ${bootpart}/ssh"
-    touch "${bootpart}/ssh"
-
     # add ssh-key to root user
     mkdir -p "${ospart}/root/.ssh"
     cat "${ssh_key}" > "${ospart}/root/.ssh/authorized_keys"
     chown -R 0:0 "${ospart}/root/.ssh"
     chmod 0600 "${ospart}/root/.ssh/authorized_keys"
-
-    # Add user
-    if ! is_null "${USERNAME}"
-    then
-        log_debug "Creating: ${bootpart}/userconf.txt"
-        echo "${USERNAME}:$(openssl passwd -6 -stdin <<<"${USERPASS}")" > "${bootpart}/userconf.txt"
-    fi
 
     # configure keyboard
     if [ -n "${keymap}" ]
